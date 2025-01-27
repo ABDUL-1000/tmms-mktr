96a276fe8afeca47ce8952805d02af10b4286689
ABDUL-1000 <abdullatifgiwa2019@gmail.com>
2025-01-27 06:38:46 +0100

    fix stuffss

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { ThemeModeScript } from "flowbite-react"
 import ProtectedRoute from "@/components/ProtectedRoute";
 
 
