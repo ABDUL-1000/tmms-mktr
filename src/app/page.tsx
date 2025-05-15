e4d91a66b7674ac99ce3791389b197546f54db27
ABDUL-1000 <abdullatifgiwa2019@gmail.com>
2025-05-15 14:05:39 +0100

    update logout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import StatisticalWidge from "@/components/StatisticalWidge";
 
 import Sidebar from "@/components/SideBar";
 import Navigation from "@/components/Navbar";
+import ProtectedRoute from "@/components/ProtectedRoute";
 
 
 export default async function Home() {
@@ -11,6 +12,7 @@ export default async function Home() {
  
   return (
    <div>
+    <ProtectedRoute>
    <Navigation/>
     <div className="max-w-7xl mx-auto px-4 ">
             <div className="grid grid-cols-1 lg:grid-cols-12 gap-6 ">
@@ -24,7 +26,7 @@ export default async function Home() {
             </div>
 
   
-    
+            </ProtectedRoute>
    </div>
   );
 }
